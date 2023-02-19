685d3e44e92b984111b9eb26b85a1631a35abc7d
Bryan Navarrete <bryann@Bryans-Air.lan>
2023-02-19 10:17:26 -0800

    changed structure

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <Text>
+      this is cool
+    </Text>
     </View>
+   
+
+    
+
   );
 }
 
